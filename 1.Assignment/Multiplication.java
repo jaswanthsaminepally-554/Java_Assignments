acd862f430ce52ae58cfcde0788631b3312aff3e
Jaswanth Saminepally <jaswanthsaminepally@gmail.com>
2025-09-06 16:06:38 +0530

    Add files via upload

---
+++ 1.Assignment/Multiplication.java
@@ -0,0 +1,14 @@
+//Multipication upto 10 numbers
+import java.util.Scanner;   
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Multiplication table for " + num + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+        sc.close();
+    }
+}
